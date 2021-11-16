4ad8267c5b0a34fff33b35d9854cf677d4bd9868
wendy herazo 2019114071 <wendyherazo@unimagdalena.edu.co>
2021-11-15 19:59:04 -0500

    Productos

---
+++ app-ejercicio/src/utils/productList.js
@@ -0,0 +1,11 @@
+export  const productList=[
+    {id: 1, nombre: "Buffalo - Striploin",precio: 39.11 },
+    {id: 2,nombre: "Bacardi Breezer - Tropical",precio: 257.92},
+    {id: 3,nombre: "Wine - Gato negro Cabernet",precio: 51.01},  
+    {id: 4,nombre: "Cabbage - Nappa", precio: 250.90},
+    {id: 5,nombre: "Sping Loaded Cup Dispenser",   precio: 175.85},
+    {id: 6,nombre: "Bread - Malt",  precio: 82.61},
+    {id: 7,nombre: "Glass Clear 8 Oz", precio: 201.10},
+    {id: 8,nombre: "Sour Puss Raspberry", precio: 134.99},
+    {id: 9,nombre: "Pork - Chop, Frenched", precio: 199.16}
+]
